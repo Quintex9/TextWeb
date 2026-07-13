759977d37ecf3dd3495e98919aa5d598857b604c
Quintex9 <misko.smatana@gmail.com>
2026-07-13 16:13:02 +0200

    Prvá verzia, header, buttons, chat

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,63 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
+  const [activeButton, setActiveButton] = useState("chat");
+
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="flex min-h-screen flex-col bg-gray-200">
+
+      <header className="flex flex-col justify-center h-15 bg-gray-300 shadow-xl">
+        <h1 className="text-xl font-bold  text-gray-800 p-4">
+          Text test web
+        </h1>
+      </header>
+
+      <section className="flex flex-col justify-center items-center pt-2">
+        <h2 className="text-2xl font-bold text-gray-800 mb-2">
+          Vítaj v prototype textového webu
+        </h2>
+        <p className="text-gray-700 text-center max-w-md">
+          Nižšie máš sekciu kde môžeš písať
+        </p>
+      </section>
+
+
+      <section className="flex flex-row justify-center items-center pb-2">
+        <button className={`w-20 h-10 rounded-2xl mt-3 mx-1 ${activeButton === "chat" ? "bg-blue-600 text-white" : "bg-blue-500"}`}
+          onClick={() => setActiveButton("chat")}>
+          Chat
+        </button>
+        <button className={`w-20 h-10 rounded-2xl mt-3 mx-1 ${activeButton === "groups" ? "bg-blue-600 text-white" : "bg-blue-500"}`}
+          onClick={() => setActiveButton("groups")}>
+          Skupiny
+        </button>
+      </section>
+
+      <section className="flex min-h-120 w-1/3 flex-row self-center justify-items-start shadow-md rounded-3xl">
+        {activeButton === "chat" && (
+          <div className="flex flex-col w-full pt-3 gap-2">
+            <div className={`flex ml-2 hover:bg-gray-300 hover:rounded-2xl mr-2`}>
+              <Image src="/chat/placeholder.svg" alt="Chat placeholder" width={50} height={50} />
+              <div>
+                <h3 className="text-md font-bold text-gray-800 translate-y-0.5 pl-2">Náhodná osoba</h3>
+                <p className="text-gray-700 text-sm pl-2">Čau</p>
+              </div>
+            </div>
+            <div className={`flex ml-2 hover:bg-gray-300 hover:rounded-2xl mr-2`}>
+              <Image src="/chat/placeholder.svg" alt="Chat placeholder" width={50} height={50} />
+              <div>
+                <h3 className="text-md font-bold text-gray-800 translate-y-0.5 pl-2">Náhodná osoba 2</h3>
+                <p className="text-gray-700 text-sm pl-2">Čau</p>
+              </div>
+            </div>
+
+            <p className="text-gray-700 text-sm text-center pt-20">Ak chceš niekoho nájsť použi search!</p>
+          </div>
+        )}
+      </section>
+    </main>
   );
 }
